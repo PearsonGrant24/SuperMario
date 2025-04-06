4b6e80f2555c08b10d57ff87d46687dad995bea6
PearsonGrant24 <100286838+PearsonGrant24@users.noreply.github.com>
2025-04-06 17:14:09 +0200

    Update backend.tf bucket name

--- EKS-Files/backend.tf
+++ EKS-Files/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ajay-mrcloudbook777" # Replace with your actual S3 bucket name
+    bucket = "pear-cloud-mario-backend129" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
